45980eaf3209633154acf57c152104fcded04b9e
prprpgate23qwa@yahoo.co.jp <prprpgate23qwa@yahoo.co.jp>
2018-07-31 12:58:30 +0900

    挨拶を追加その３

--- Exec.java
+++ Exec.java
@@ -2,5 +2,7 @@ package exsample01;
 public class Exec {
 	public static void main (String[] args){
 		System.out.println("挨拶:おはよう");
+		System.out.println("挨拶:こんにちは");
+		System.out.println("挨拶:こんばんは");
 	}
 }
